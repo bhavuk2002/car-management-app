6bf57a808ca819a59380170c12ca779e758137ee
Bhavuk Mittal <vvbnmittal@gmail.com>
2024-11-16 23:16:57 +0530

    all working in deployment

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,3 +1,5 @@
-const baseURL = process.env.REACT_APP_API_URL || "http://localhost:5000";
+const baseURL =
+  process.env.REACT_APP_API_URL ||
+  "https://car-management-api-yy1q.onrender.com";
 
 export default baseURL;
