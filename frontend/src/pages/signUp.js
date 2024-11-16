e9a46c134dbcd7f43d5ef01083284c97b3ec6e43
Bhavuk Mittal <vvbnmittal@gmail.com>
2024-11-16 22:35:52 +0530

    updated

--- frontend/src/pages/signUp.js
+++ frontend/src/pages/signUp.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import baseURL from "../utils/config";
 
 const Signup = () => {
   const [formData, setFormData] = useState({
