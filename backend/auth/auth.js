48573c643c5a6cc71e19fba78cedb3499b2f54b2
Bhavuk Mittal <vvbnmittal@gmail.com>
2024-11-16 21:48:25 +0530

    changes

--- backend/auth/auth.js
+++ backend/auth/auth.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const User = require("../models/user");
+const User = require("../models/User.js");
 const jwt = require("jsonwebtoken");
 
 const auth = async (req, res, next) => {
